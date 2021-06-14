ae2b9f6fc0456ba7e1d3dc7bd9c3a26da9b64584
Ellen Wang <Ellen.Wang@ibm.com>
2021-06-14 14:33:10 -0400

    Issue 4310: Updated landing page UI and Swagger url

--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -1,12 +1,16 @@
 package application.rest.v1;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+
 import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-//import javax.ws.rs.core.Response.Status;
 import java.util.ArrayList;
 
 
@@ -14,9 +18,17 @@ import java.util.ArrayList;
 public class Example {
 
 
-
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Operation(summary = "Get status of application")
+    @APIResponses(
+            value = {
+                    @APIResponse(
+                            responseCode = "200",
+                            description = "Confirmation message that application is running",
+                            content = @Content(mediaType = "text/plain"))
+            }
+    )
     public Response example() {
         List<String> list = new ArrayList<>();
         //return a simple list of strings
